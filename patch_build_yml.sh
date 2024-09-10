a99d5f6bd4691565f4abbd30b2517ddaecd4733f
Arcadiy Ivanov <arcadiy@ivanov.biz>
2024-09-09 20:52:31 -0400

    Try multiarch builds

--- patch_build_yml.sh
+++ patch_build_yml.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eEu
 
+pushd manylinux
+git restore .github/workflows/build.yml
+popd
+
 cp manylinux/.github/workflows/build.yml .github/workflows/build.yml
 patch -p1 < build.yml.patch
 
