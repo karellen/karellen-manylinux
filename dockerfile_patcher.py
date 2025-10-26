5341f6695b4b3cf96aefe6a79f365a46e4dd9e71
Arcadiy Ivanov <arcadiy@ivanov.biz>
2025-10-26 11:30:08 -0400

    Fix Dockerfile patching

--- dockerfile_patcher.py
+++ dockerfile_patcher.py
@@ -6,25 +6,34 @@ with open(sys.argv[1], "rt") as f:
     lines = list(l.rstrip() for l in f.readlines())
 
 new_lines = []
-
+no_img_defs_patched = True
+no_img_refs_patched = True
+ignore_lines = 0
 for idx, line in enumerate(lines):
-    if line.startswith("RUN manylinux-entrypoint /build_scripts/build-cpython.sh"):
-        cpython_build_block = [line, ""]
-        if lines[idx-1].startswith("COPY"):
-            cpython_build_block.extend(lines[idx-2:idx+1])
-        else:
-            cpython_build_block.extend(lines[idx-1:idx+1])
-        cpython_build_block[2] += "_shared"
+    if ignore_lines:
+        ignore_lines -= 1
+        continue
+    if line.startswith("FROM build_cpython AS build_cpython"):
+        cpython_build_block = lines[idx:idx+4] + [""]
+        cpython_build_block.extend(lines[idx:idx+4])
+        cpython_build_block[5] += "_shared"
         cpython_build_block[-1] += " shared"
         new_lines.extend(cpython_build_block)
+        ignore_lines = 3
+        no_img_defs_patched = False
     elif "--mount=type=bind,target=/build_cpython" in line:
         new_lines.append(line)
         if line.startswith("RUN"):
             line = "    " + line[4:]
         part1, part2 = line.split(",from=")
         new_lines.append(part1 + "_shared" + ",from=" + part2.replace(" \\", "_shared \\"))
+        no_img_refs_patched = False
     else:
         new_lines.append(line)
+
+if no_img_defs_patched or no_img_refs_patched:
+    print("Failed patching Dockerfile!", file=sys.stderr)
+    sys.exit(1)
 
 with open(sys.argv[1], "wt") as f:
     f.write("\n".join(new_lines))
